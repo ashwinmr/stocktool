d31413153eb63c95aaeb510e8c1b53f6259b63db
ashwinmr <iashwinrao@gmail.com>
2019-12-26 23:24:22 +0530

    add tickers

--- main.py
+++ main.py
@@ -31,7 +31,18 @@ TICKERS = [
 'VTI',
 'VOO',
 'AMAT',
-'BYD',
+'BYDDY',
+'GM',
+'FCAU',
+'GOOG',
+'UBER',
+'LYFT',
+'T',
+'FDX',
+'BIDU',
+'SNAP',
+'QCOM',
+'BYND',
 ]
 
 def download_data(start = dt.datetime(2000,1,1), end = dt.datetime(2019,12,26), tickers = TICKERS, force = False):
@@ -104,7 +115,7 @@ def create_value_legend(tickers,values):
 if __name__ == "__main__":
 
     # Set start and end
-    start = dt.datetime(2018,1,1)
+    start = dt.datetime(2019,1,1)
     end = dt.datetime(2019,12,26)
 
     # Download data
